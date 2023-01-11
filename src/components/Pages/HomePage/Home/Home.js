bd51ddfaf5db9e84433e79abf78eef8b8e3fef0a
Md. Harun OR Rashid <108585636+harunrashid4419@users.noreply.github.com>
2023-01-12 00:16:46 +0600

    customer page

--- src/components/Pages/HomePage/Home/Home.js
+++ src/components/Pages/HomePage/Home/Home.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Contact from "../Contact/Contact";
+import CustomerFeedBack from "../CustomerFeedback/CustomerFeedBack";
 
 const Home = () => {
   return (
     <div>
       <Contact></Contact>
-      <p>this is toukir branch</p>
-      <h1>added by masum</h1>
-      <p>This is another components</p>
+      <CustomerFeedBack></CustomerFeedBack>
     </div>
   );
 };
