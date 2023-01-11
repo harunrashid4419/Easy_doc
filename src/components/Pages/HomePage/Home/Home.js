79ca2518c14836c425972d915ac4866910fd0b70
Masum Billah <masum.monpura@gmail.com>
2023-01-11 23:32:46 +0600

    added by masum

--- src/components/Pages/HomePage/Home/Home.js
+++ src/components/Pages/HomePage/Home/Home.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import Contact from '../Contact/Contact';
+import React from "react";
+import Contact from "../Contact/Contact";
 
 const Home = () => {
-    return (
-        <div>
-            <Contact></Contact>
-        </div>
-    );
+  return (
+    <div>
+      <Contact></Contact>
+      <h1>added by masum</h1>
+    </div>
+  );
 };
 
 export default Home;
