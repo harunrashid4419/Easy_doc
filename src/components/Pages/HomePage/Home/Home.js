1cd3a28991a82a906c2ae7f81acc9275b669b555
polyakhtar <poly09876pm@gmail.com>
2023-01-12 18:42:59 +0600

    banner designed

--- src/components/Pages/HomePage/Home/Home.js
+++ src/components/Pages/HomePage/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "../Banner/Banner";
 import Contact from "../Contact/Contact";
 import CustomerFeedBack from "../CustomerFeedback/CustomerFeedBack";
 
 const Home = () => {
   return (
     <div>
+      <Banner></Banner>
       <CustomerFeedBack></CustomerFeedBack>
       <Contact></Contact>
     </div>
