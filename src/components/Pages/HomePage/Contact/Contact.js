b7fb897b9cef912f78f764f4fd1fa93b094e5ab8
Md. Harun OR Rashid <108585636+harunrashid4419@users.noreply.github.com>
2023-01-12 14:46:39 +0600

    polise some code

--- src/components/Pages/HomePage/Contact/Contact.js
+++ src/components/Pages/HomePage/Contact/Contact.js
@@ -5,18 +5,20 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div className="main-contact-section">
-      <div className="contact-section">
-        <h2>Get In Touch</h2>
-        <input type="text" placeholder="Your Name" className="input w-full" />
-        <input
-          id="email-section"
-          type="email"
-          placeholder="Your Email"
-          className="input w-full"
-        />
-        <textarea className="textarea" placeholder="Your Message"></textarea>
-        <div className="button">
-          <Link>Send Message</Link>
+      <div className="container">
+        <div className="contact-section">
+          <h2>Get In Touch</h2>
+          <input type="text" placeholder="Your Name" className="input w-full" />
+          <input
+            id="email-section"
+            type="email"
+            placeholder="Your Email"
+            className="input w-full"
+          />
+          <textarea className="textarea" placeholder="Your Message"></textarea>
+          <div className="button">
+            <Link>Send Message</Link>
+          </div>
         </div>
       </div>
     </div>
