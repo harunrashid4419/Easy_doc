af5f5af29ef1b2dc2464c84b95fb50c9d9f02cbb
ashr-aful007 <ashraful99980@gmail.com>
2023-01-13 14:59:58 +0600

    navbar-rechange

--- src/components/SharedPage/Navbar/Navbar.js
+++ src/components/SharedPage/Navbar/Navbar.js
@@ -2,13 +2,10 @@ import React, { useState } from 'react';
 import {
   FaBars,
   FaTimes,
-  FaGithub,
-  FaLinkedin,
-  FaFacebook,
-  FaLinkedinIn,
+  FaRegListAlt,
+  FaUsersCog,
+
 } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 
@@ -17,9 +14,9 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed  z-50 w-full h-[60px] flex justify-between items-center px-4  text-gray-300'>
+    <div className='fixed bg-[#7846E9] z-50 w-full h-[60px] flex  items-center px-4  text-gray-300'>
       <div>
-        <p className='text-2xl font-semibold text-white'>Easy Doc</p> 
+        <p className='text-2xl font-semibold text-white md:mr-24'>Easy Doc</p> 
       </div>
 
       {/* menu */}
@@ -54,9 +51,10 @@ const Navbar = () => {
         className={
           !nav
             ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center lg:hidden'
         }
       >
+        <button className='w-full flex-col'></button>
         <li className='py-6 text-4xl'>
         <Link to='/'>Home</Link>
         </li>
@@ -80,20 +78,20 @@ const Navbar = () => {
       {/* Social icons */}
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+          <li className='w-[160px]  h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
-              className='flex justify-between items-center w-full text-gray-300'
+              className='flex ml-4 mr-4 justify-between items-center w-full text-gray-300'
               href='/'
             >
-              Linkedin <FaLinkedin size={30} />
+              Document <FaRegListAlt size={25} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
-              className='flex justify-between items-center w-full text-gray-300'
+              className='flex ml-4 mr-4 justify-between items-center w-full text-gray-300'
               href='/'
             >
-              Github <FaGithub size={30} />
+              Community <FaUsersCog size={30} />
             </a>
           </li>
         </ul>
