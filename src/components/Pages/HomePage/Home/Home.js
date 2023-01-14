559bb3472b6bd77b5bb772b7679f98daa7496b8c
Masum Billah <masum.monpura@gmail.com>
2023-01-14 21:32:28 +0600

    created a new component

--- src/components/Pages/HomePage/Home/Home.js
+++ src/components/Pages/HomePage/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Card from "../Card/Card";
+import RecommendedTopics from "../Card/RecommendedTopics";
 import Contact from "../Contact/Contact";
 import CustomerFeedBack from "../CustomerFeedback/CustomerFeedBack";
 
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <CustomerFeedBack></CustomerFeedBack>
+      <RecommendedTopics></RecommendedTopics>
       <Contact></Contact>
       <Card></Card>
     </div>
