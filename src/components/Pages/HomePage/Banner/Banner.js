2bb764709efa4f91220c05ff26123e3072e3d024
polyakhtar <poly09876pm@gmail.com>
2023-01-18 11:38:56 +0600

    json file added

--- src/components/Pages/HomePage/Banner/Banner.js
+++ src/components/Pages/HomePage/Banner/Banner.js
@@ -42,10 +42,6 @@ const Banner = () => {
               className="px-4 block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 py-4 rounded-full text-lg"
               placeholder="Search For Topics"
             />
-
-            <span className=" text-white absolute right-2.5 bottom-2.5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              All
-            </span>
           </div>
         </form>
       </div>
