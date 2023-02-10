5eb762122806c0029929a2b82f7c1d61477279cc
ashr-aful007 <ashraful99980@gmail.com>
2023-02-10 12:47:24 +0600

    payment section api implement successfuly

--- src/Payment/CheckoutForm.js
+++ src/Payment/CheckoutForm.js
@@ -1,11 +1,35 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
-import React from 'react'
+import React, { useContext } from 'react'
+import { useEffect } from 'react';
 import { useState } from 'react'
+import { AuthContext } from '../Context/UserContext';
+
 
 function CheckoutFrom() {
+  const {user} = useContext(AuthContext)
     const [cardError, setCardError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [processing, setProcessing] = useState(false);
+    const [transactionId, setTransactionId] = useState('');
+    const [clientSecret, setClientSecret] = useState("")
      const stripe = useStripe()
      const elements = useElements()
+
+     //subscription amount price
+     const price = 2;
+
+     useEffect(() =>{
+        fetch("http://localhost:5000/create-payment-intent",{
+          method: "POST",
+          headers: {
+            "Content-Type" : "application/json",
+            // authorization: `bearer ${localStorage.getItem('accessToken')}`
+          },
+          body: JSON.stringify({price}),
+        })  
+        .then((res) => res.json())
+        .then((data) => setClientSecret(data.clientSecret))
+     },[price])
 
      const handleSubmit = async(event) =>{
           event.preventDefault();
@@ -27,6 +51,33 @@ function CheckoutFrom() {
           else{
             setCardError('');
           }
+          //reset success
+          setSuccess('');
+          setProcessing(true);
+          //card payment confirm
+          const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+              payment_method: {
+                card: card,
+                billing_details: {
+                  name: user?.displayName,
+                  email: user?.email
+                },
+              },
+            },
+          );
+          if(confirmError){
+            setCardError(confirmError.message);
+            return;
+          }
+          if(paymentIntent.status === "succeeded"){
+            setSuccess('Congrats! your payment completed');
+            setTransactionId(paymentIntent.id);
+            
+          }
+          setProcessing(false);
+          console.log(paymentIntent)
      }
   return (
     <>
@@ -47,11 +98,17 @@ function CheckoutFrom() {
          },
        }}
      />
-     <button className='btn btn-sm mt-4 btn-primary' type="submit" disabled={!stripe}>
+     <button className='btn btn-sm mt-4 btn-primary' type="submit" disabled={!stripe || !clientSecret || processing}>
        Pay
      </button>
    </form>
    <p className="text-red-500">{cardError}</p>
+   {
+     success && <div>
+      <p className='text-green-500'>{success}</p>
+      <p>Your transactionId: <span className='font-bold'>{transactionId}</span></p>
+     </div>
+   }
     </>
   )
 }
