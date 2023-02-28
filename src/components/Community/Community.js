95052395eb636a25212d90ffb86b3c92691a7f2c
ashr-aful007 <ashraful99980@gmail.com>
2023-02-28 12:08:20 +0600

    fixed

--- src/components/Community/Community.js
+++ src/components/Community/Community.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import Post from "./Post";
+import ShowPost from "./ShowPost";
 
 const Community = () => {
 
   return (
     <div className="container">
       <Banner></Banner>
-      <Post></Post>
+      <ShowPost></ShowPost>
     </div>
   );
 };
--- src/components/Community/ShowPost.js
+++ src/components/Community/ShowPost.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import ShowSinglePost from "./ShowSinglePost";
+import { useState } from "react";
+import Post from "./Post";
 
 const ShowPost = () => {
-  const { data: allpost = [] } = useQuery({
+  
+  const { data: allpost = [],refetch } = useQuery({
     queryKey: ["post"],
     queryFn: async () => {
       const res = await fetch("https://easy-doc-server.vercel.app/allUserPost");
       const data = await res.json();
       return data;
+      
     },
   });
 
   return (
     <div>
+    <Post
+    refetch={refetch}
+    ></Post>
       {allpost.map((singlePost) => (
         <ShowSinglePost
           key={singlePost._id}
