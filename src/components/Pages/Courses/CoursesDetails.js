ae221f0d4751ad46c4be0a89ae59f23af801c893
ashr-aful007 <ashraful99980@gmail.com>
2023-02-28 16:26:23 +0600

    courses details data setup

--- src/components/Pages/Courses/CoursesDetails.js
+++ src/components/Pages/Courses/CoursesDetails.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function CoursesDetails() {
+  const coursesDetails= useLoaderData()
+  const {coursesName,fullderscription,heading,img,participant,price,_id} = coursesDetails;
+  console.log(_id);
+
+
+  
   return (
-    <div>CoursesDetails</div>
+    <div>
+      <p>{}</p>
+    </div>
   )
 }
 
